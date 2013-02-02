efc3a0764b362dc383fcd0b2101d4818171bacdf
gareth <gareth@PaloAlto.(none)>
2013-02-02 18:45:31 -0500

    Getting the video title from youtube.

--- handlers/home.py
+++ handlers/home.py
@@ -4,6 +4,7 @@ import tornado.web
 from settings import settings
 from base import *
 import requests
+import re
 
 class HomeHandler(BaseHandler):
     """
@@ -12,8 +13,25 @@ class HomeHandler(BaseHandler):
         if video == '':
             self.render_template("home.html")
         else:
+            title = self.get_video_title(video)
+
             self.render_template("video.html",
                 args={
-                    'video': video
+                    'video': video,
+                    'title': title,
             })
+
+    def get_video_title(self, video):
+        resource_data = requests.get('http://gdata.youtube.com/feeds/api/videos/' + video) 
+
+        match = re.search(
+            "<media:title.*?>(?P<title>.*)<\/media:title>", 
+            resource_data.text)
+
+        logging.info("match: " + str(match.group('title')))
+        title = match.group('title') 
+
+        return title
+        
+
 
